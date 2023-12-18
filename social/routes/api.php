6f40fde0689ff5e7d8f4c8e141d2462c6d491b61
Abdul-karem <126398799+Abdul-karem@users.noreply.github.com>
2023-12-18 09:35:41 +0300

    yasmeens file

--- social/routes/api.php
+++ social/routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ReportController;
+use App\Http\Controllers\AnalyticsController;
 use App\Http\Controllers\Api\MalekController;
+use App\Http\Controllers\ModeratorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +25,19 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/friendRequest',[MalekController::class,'friendRequest']);
 Route::post('/respondToFriendRequest',[MalekController::class,'respondToFriendRequest']);
 Route::post('/updateUser/{id}',[MalekController::class,'updateUser']);
+
+// route for creating report from user
+Route::post('/reports/{UserID}/{PostID}', [ReportController::class, 'makeReport']);
+// route for handling moderator response on reports
+Route::get('/reports', [ModeratorController::class, 'reports']);
+Route::get('/reports/{ReporID}', [ModeratorController::class, 'showReport']);
+Route::delete('/reports/{ReporID}', [ModeratorController::class, 'deleteReport']);
+Route::put('/reports/{ReporID}/accept', [ModeratorController::class, 'acceptReport']);
+// analytic routes
+Route::get('/analytics/likes', [AnalyticsController::class, 'getLikes']);
+Route::get('/analytics/comments', [AnalyticsController::class, 'getComments']);
+Route::get('/analytics/report', [AnalyticsController::class, 'analyticsReport']);
+// routes for creating, updating, deleting user
+Route::post('/', [UserController::class, 'createUser']);
+Route::put('/{id}', [UserController::class, 'updateUser']);
+Route::delete('/{id}', [UserController::class, 'deleteUser']);
