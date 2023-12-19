6388c5111185247768eeeb2132c4a540dbeff7fd
MalekAlobeidat <obe.malek@gmail.com>
2023-12-19 21:53:13 +0300

    malek

--- social/app/Http/Controllers/ReportController.php
+++ social/app/Http/Controllers/ReportController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Report;
 use App\Models\Post;
 use App\Models\Privacysetting;
+use App\Models\Report;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ReportController extends Controller
@@ -18,9 +19,13 @@ class ReportController extends Controller
      */
     public function makeReport($UserID, $PostID)
     {
-        $report = Report::where('UserID', $UserID)->where('PostID', $PostID)->get();
-    
-        if ($report->isNotEmpty()) {
+    // Check if there is a report for the same user, post, and date
+    $existingReport = Report::where('UserID', $UserID)
+                            ->where('PostID', $PostID)
+                            ->whereDate('created_at', Carbon::today())
+                            ->first();
+
+        if ($existingReport !== null && $existingReport->count() > 0) {
             return response()->json(['message' => 'already reported'], 201);
         }
         $report = new Report();
